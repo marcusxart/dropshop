b308a52c8c427dbaae27458a9bbcbba0574f3930
Dhanielgeek <dhanielknightz46@gmail.com>
2024-10-08 23:24:28 +0100

    made the responsive on the Register

--- app/src/components/authWrapper.jsx
+++ app/src/components/authWrapper.jsx
@@ -7,7 +7,7 @@ const AuthWrapper = () => {
       <div className="flex-1 pt-[90px]">
         <div className="max-w-[380px] w-full mx-auto pt-[48px] pb-[24px] flex flex-col gap-[32px] items-center">
           <div className="border-t border-[#23252A] w-full "></div>
-          <div className="flex-1 w-full">
+          <div className="flex-1 w-full flex justify-center items-center">
             <Outlet />
           </div>
         </div>
