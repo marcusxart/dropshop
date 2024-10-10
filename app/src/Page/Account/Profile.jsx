17458a7f4f110b5730ca29f698a8733092562519
Dhanielgeek <dhanielknightz46@gmail.com>
2024-10-10 22:05:57 +0100

    Trying something neww...

--- app/src/Page/Account/Profile.jsx
+++ app/src/Page/Account/Profile.jsx
@@ -1,5 +1,82 @@
+import { FaRegEdit } from "react-icons/fa";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center scrollbar-hide overflow-y-scroll">
+      <div className="w-[50%] h-[90%] bg-[#0B0C0D] rounded-lg max-md:w-[90%] max-md:h-[80%]">
+        <div className="w-full h-[20%]  flex justify-center gap-2 items-center max-md:h-[30%]">
+          <div className="w-[90px] h-[90px] rounded-full border">
+            <img src="" alt="" />
+          </div>
+
+          <div className="w-[30px] h-[30px] bg-[#f8c534] rounded-full flex justify-center items-center">
+            <FaRegEdit size={20} className="text-black" />
+          </div>
+        </div>
+        <div className="w-full h-[20%] flex justify-around items-center">
+          <div className="w-[40%] h-[60%] flex flex-col justify-center items-start gap-1 max-md:h-[70%] ">
+            <label htmlFor="" className=" font-semibold text-gray-500">
+              First Name
+            </label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Jessica"
+              readOnly
+              className="w-full h-[90%] border rounded-md bg-transparent px-2 outline-none"
+            />
+          </div>
+          <div className="w-[40%] h-[60%] flex flex-col justify-center items-start gap-1 max-md:h-[70%]">
+            <label htmlFor="" className=" font-semibold text-gray-500">
+              Last name
+            </label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Ibu"
+              readOnly
+              className="w-full h-[90%] border rounded-md bg-transparent px-2 outline-none"
+            />
+          </div>
+        </div>
+        <div className="w-full h-[20%]   flex justify-around items-center">
+          <div className="w-[40%] h-[60%] flex flex-col justify-center items-start gap-1 max-md:h-[70%]">
+            <label htmlFor="" className=" font-semibold text-gray-500">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="1234567890"
+              readOnly
+              className="w-full h-[90%] border rounded-md bg-transparent px-2 outline-none"
+            />
+          </div>
+          <div className="w-[40%] h-[60%] flex flex-col justify-center items-start gap-1 max-md:h-[70%]">
+            <label htmlFor="" className=" font-semibold text-gray-500">
+              Email
+            </label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="jes@example.com"
+              readOnly
+              className="w-full h-[90%] border rounded-md bg-transparent px-2 outline-none"
+            />
+          </div>
+        </div>
+        <div className="w-full h-[20%] flex justify-center items-center">
+          <button className="px-10 py-3 rounded-full text-black font-semibold bg-slate-300">
+            Save Changes
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
