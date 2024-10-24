080cdcc9197e799409ff2953d236ea542604cee1
Dhanielgeek <dhanielknightz46@gmail.com>
2024-10-24 03:06:24 +0100

    started the order part...

--- app/src/components/Modals/Pickdetails.jsx
+++ app/src/components/Modals/Pickdetails.jsx
@@ -1,30 +1,33 @@
 import { PiBuildingOfficeBold } from "react-icons/pi";
 import { MdHome, MdLocationOn, MdPhone } from "react-icons/md";
 
-const Pickdetails = () => {
+const Pickdetails = ({ location }) => {
   return (
-    <div className="w-[40rem] h-[30rem] bg-black max-md:w-[23rem]">
-      <div className="w-full h-[15%] flex justify-start px-7 items-center">
-        <p className=" font-semibold text-2xl max-md:text-xl">
-          Add Pickup Details
-        </p>
-      </div>
-      <div className="w-full h-[28%] flex justify-center items-center">
-        <div className="w-[90%] h-[90%]  hover:border-[#f8c534] flex items-center  border border-[#0b0c0d] bg-[#0b0c0d] rounded">
-          <div className="w-[13%] h-full flex justify-center items-start py-2">
-            <PiBuildingOfficeBold size={25} className="text-[#f8c534]" />
-          </div>
-          <div className="w-[70%] h-full  px-2  flex-col flex justify-center gap-2 items-start">
-            <p className=" font-semibold">Buss</p>
-            <p className=" text-sm text-gray-600 font-medium">
-              Someshwarpura, Bengaluru, Karnataka, India, 560008
-            </p>
-            <button className=" px-6 py-[6px] font-semibold text-black bg-slate-200 rounded-full">
-              Edit map
-            </button>
-          </div>
-          <div className="w-[16%] h-full  flex justify-center items-center">
-            <MdLocationOn size={30} className="text-[#f8c534]" />
+    <>
+      <div className="w-[40rem] h-[30rem] bg-black max-md:w-[23rem]">
+        <div className="w-full h-[15%] flex justify-start px-7 items-center">
+          <p className="font-semibold text-2xl max-md:text-xl">
+            Add Pickup Details
+          </p>
+        </div>
+        <div className="w-full h-[28%] flex justify-center items-center">
+          <div className="w-[90%] h-[90%] hover:border-[#f8c534] flex items-center border border-[#0b0c0d] bg-[#0b0c0d] rounded">
+            <div className="w-[13%] h-full flex justify-center items-start py-2">
+              <PiBuildingOfficeBold size={25} className="text-[#f8c534]" />
+            </div>
+            <div className="w-[70%] h-full px-2 flex-col flex justify-center gap-2 items-start">
+              <p className="font-semibold">Business</p>
+              <p className="text-sm text-gray-600 font-medium">
+                {location ||
+                  "Someshwarpura, Bengaluru, Karnataka, India, 560008"}
+              </p>
+              <button className="px-6 py-[6px] font-semibold text-black bg-slate-200 rounded-full">
+                Edit map
+              </button>
+            </div>
+            <div className="w-[16%] h-full flex justify-center items-center">
+              <MdLocationOn size={30} className="text-[#f8c534]" />
+            </div>
           </div>
         </div>
       </div>
@@ -105,7 +108,7 @@ const Pickdetails = () => {
           </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
