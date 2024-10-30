0d7f353095f0435b57c38550c055ef2f0bc83703
Dhanielgeek <dhanielknightz46@gmail.com>
2024-10-30 12:22:58 +0100

    testing on my branch

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,202 +1,425 @@
-const {Orders} = require("../database")
-const {getIo,onlineCustomers}=  require("../utils/socketConnection")
-const {Op} = require("sequelize")
-const appError = require("../utils/appError")
+// const {Orders} = require("../database")
+// const {getIo,onlineCustomers}=  require("../utils/socketConnection")
+// const {Op} = require("sequelize")
+// const appError = require("../utils/appError")
 
-const createOrder=async(req,res,next)=>{
+// const createOrder=async(req,res,next)=>{
 
-const {type,price,from,to,number,details}= req.body
-const customerName = req.user.name
-try {
-    if (!type || !details || !price || !from || !to || !number ) {
-        const err = new Error("fill in all required fields")
-        err.status = 400
-        return next(err)
+// const {type,price,from,to,number,details}= req.body
+// const customerName = req.user.name
+// try {
+//     if (!type || !details || !price || !from || !to || !number ) {
+//         const err = new Error("fill in all required fields")
+//         err.status = 400
+//         return next(err)
+//     }
+//     const createOrder = await Orders.create({type,price,from,to,details,number,customer:customerName})
+//     if (createOrder) {
+//         res.status(200).json(createOrder)
+//     }
+// } catch (error) {
+//     const err = new Error(error.message)
+//     return next(err)
+// }
+
+// }
+
+// const acceptOrder=async(req,res,next)=>{
+
+//  const {id} = req.params
+//  const riderName = req.user.name
+//  const riderNumber = req.user.riderNumber
+
+//  try {
+//   const busyRider = await Orders.findAll({where:{status:"in-progress",rider:riderName}})
+//   if (busyRider.length!==0) {
+//     const err = new appError("rider already occupied",400)
+//     return next(err)
+//   }
+//   const findOrder = await Orders.findByPk(id)
+
+//     if (!findOrder) {
+//         const err = new Error("order not found")
+//         err.status= 404
+//         return next(err)
+//     }
+//     findOrder.rider = riderName
+//     findOrder.status = "in-progress"
+//     findOrder.riderNumber=riderNumber
+//     findOrder.stage=1
+//     await findOrder.save()
+//     //emit io
+//    res.status(200).json(findOrder)
+
+//  } catch (error) {
+//    const err = new Error(error.message)
+//    return next(err)
+//  }
+
+// }
+
+// const getAllOrders =async(req,res,next)=>{
+//   const status = req.query.status
+//   queryOptions = {}
+//   if (status) {
+//     sequelizeOptions.where=  {status:{[Op.eq]:status}}
+//   }
+// try {
+//    const getOrders = await Orders.findAll(queryOptions)
+//    if (!getOrders) {
+//     return res.status(404).json("No orders yet")
+//   }
+//   res.status(200).json(getOrders)
+// } catch (error) {
+//     const err = new Error(error.message)
+//     return next(err)
+// }
+// }
+// const getPendingOrders=async(req,res,next)=>{
+// try {
+//   const pending= await Orders.findAll({where:{status: "pending"}})
+//   if (!pending) {
+//     return res.status(404).json("No pending order")
+//   }
+//   res.status(200).json(pending)
+// } catch (error) {
+//   const err = new Error(error.message)
+//   return next(err)
+// }
+// }
+// const getOrderById =async(req,res,next) =>{
+//     const {id} = req.params
+//  try {
+//     const order = await Orders.findByPk(id)
+
+//     if (!order) {
+//         return res.status(404).json("Order not found")
+//       }
+//       res.status(200).json(order)
+
+//  } catch (error) {
+//     const err = new Error(error.message)
+//     return next(err)
+//  }
+// }
+
+// let orderUpdate
+
+// const updateOrder =async(req,res,next)=>{
+//     const {id} = req.params
+//     try {
+//         const order= await Orders.update(req.body,{where:{id}})
+//         if (order.stage ===2) {
+//         //emit io
+//         }
+//        res.status(201).json("stage updated")
+//     } catch (error) {
+//       const err = new Error(error.message)
+//       return next(err)
+//     }
+// }
+
+// const riderOrderHistory = async(req,res,next)=>{
+//   const riderName = req.user.name
+//   try {
+//     const getOrderHistory= await Orders.findAll({where:{status:{[Op.ne]:"in-progress"},rider:riderName}})
+
+//     if (!getOrderHistory) {
+//       const err = new Error("No orders yet")
+//       err.status= 404
+//       return next(err)
+//     }
+//     res.status(200).json(getOrderHistory)
+//   } catch (error) {
+//     const err = new Error(error.message)
+//     return next(err)
+//   }
+// }
+
+// const customerOrderHistory = async(req,res,next)=>{
+//   const customerName = req.user.name
+//   try {
+//     const getOrderHistory= await Orders.findAll({where:{status:{[Op.ne]:"in-progress"},customer:customerName}})
+
+//     if (!getOrderHistory) {
+//       const err = new Error("No orders yet")
+//       err.status= 404
+//       return next(err)
+//     }
+//     res.status(200).json(getOrderHistory)
+//   } catch (error) {
+//     const err = new Error(error.message)
+//     return next(err)
+//   }
+// }
+
+// const riderOngoingOrder = async(req,res,next)=>{
+//   const riderName = req.user.name
+//   try {
+//     const ongoingOrder= await Orders.findOne({where:{status:"in-progress",rider:riderName}})
+
+//     if (!ongoingOrder) {
+//       const err = new Error("No orders yet")
+//       err.status= 404
+//       return next(err)
+//     }
+//     const ongoingRiderOrder= []
+//     ongoingRiderOrder.push(ongoingOrder)
+
+//     res.status(200).json(ongoingRiderOrder)
+//   } catch (error) {
+//     const err = new Error(error.message)
+//     return next(err)
+//   }
+// }
+
+// const customerOngoingOrder = async(req,res,next)=>{
+//   const customerName = req.user.name
+//   try {
+//     const ongoingOrder= await Orders.findOne({where:{status:"in-progress",customer:customerName}})
+
+//     if (!ongoingOrder) {
+//       const err = new Error("No orders yet")
+//       err.status= 404
+//       return next(err)
+//     }
+//     res.status(200).json(ongoingOrder)
+//   } catch (error) {
+//     const err = new Error(error.message)
+//     return next(err)
+//   }
+// }
+// module.exports={
+//   getAllOrders
+//   ,getOrderById,
+//   getPendingOrders,
+//   createOrder,
+//   acceptOrder,
+//   updateOrder,
+//   riderOrderHistory,
+//   riderOngoingOrder,
+//   customerOngoingOrder,
+//   customerOrderHistory
+// }
+
+const { Orders } = require("../database");
+const { getIo, onlineCustomers } = require("../utils/socketConnection");
+const { Op } = require("sequelize");
+const appError = require("../utils/appError");
+const { emitOrderStatusUpdate } = require("../helpers/scoket_helper");
+
+const createOrder = async (req, res, next) => {
+  const { type, price, from, to, number, details } = req.body;
+  const customerName = req.user.name;
+  try {
+    if (!type || !details || !price || !from || !to || !number) {
+      const err = new Error("fill in all required fields");
+      err.status = 400;
+      return next(err);
     }
-    const createOrder = await Orders.create({type,price,from,to,details,number,customer:customerName})
+    const createOrder = await Orders.create({
+      type,
+      price,
+      from,
+      to,
+      details,
+      number,
+      customer: customerName,
+    });
     if (createOrder) {
-        res.status(200).json(createOrder)
+      res.status(200).json(createOrder);
     }
-} catch (error) {
-    const err = new Error(error.message)
-    return next(err)
-}
-
-}
-
-
-const acceptOrder=async(req,res,next)=>{
-
- const {id} = req.params
- const riderName = req.user.name
- const riderNumber = req.user.riderNumber
- 
- try {
-  const busyRider = await Orders.findAll({where:{status:"in-progress",rider:riderName}})
-  if (busyRider.length!==0) {
-    const err = new appError("rider already occupied",400)
-    return next(err)
+  } catch (error) {
+    const err = new Error(error.message);
+    return next(err);
   }
-  const findOrder = await Orders.findByPk(id)
+};
+
+const acceptOrder = async (req, res, next) => {
+  const { id } = req.params;
+  const riderName = req.user.name;
+  const riderNumber = req.user.riderNumber;
+  const io = req.app.get("socketio");
+  try {
+    const busyRider = await Orders.findAll({
+      where: { status: "in-progress", rider: riderName },
+    });
+    if (busyRider.length !== 0) {
+      const err = new appError("rider already occupied", 400);
+      return next(err);
+    }
+    const findOrder = await Orders.findByPk(id);
 
     if (!findOrder) {
-        const err = new Error("order not found")
-        err.status= 404
-        return next(err)
+      const err = new Error("order not found");
+      err.status = 404;
+      return next(err);
     }
-    findOrder.rider = riderName
-    findOrder.status = "in-progress"
-    findOrder.riderNumber=riderNumber
-    findOrder.stage=1
-    await findOrder.save()
-    //emit io
-   res.status(200).json(findOrder) 
+    // findOrder.rider = riderName;
+    // // findOrder.status = "in-progress";
+    // findOrder.riderNumber = riderNumber;
+    // findOrder.stage = 1;
+    // // await findOrder.save();
 
- } catch (error) {
-   const err = new Error(error.message)
-   return next(err) 
- }
+    console.log("Customer Name:", findOrder.customer);
+    console.log(io, "sockect io");
 
-} 
+    emitOrderStatusUpdate({
+      io,
+      customerName: findOrder.customer,
+      role: "customer",
+      orderStatus: findOrder.status,
+      data: findOrder,
+    });
 
-const getAllOrders =async(req,res,next)=>{
-  const status = req.query.status
-  queryOptions = {}
+    res.status(200).json(findOrder);
+  } catch (error) {
+    const err = new Error(error.message);
+    return next(err);
+  }
+};
+
+const getAllOrders = async (req, res, next) => {
+  const status = req.query.status;
+  queryOptions = {};
   if (status) {
-    sequelizeOptions.where=  {status:{[Op.eq]:status}}
+    sequelizeOptions.where = { status: { [Op.eq]: status } };
   }
-try {
-   const getOrders = await Orders.findAll(queryOptions) 
-   if (!getOrders) {
-    return res.status(404).json("No orders yet")
+  try {
+    const getOrders = await Orders.findAll(queryOptions);
+    if (!getOrders) {
+      return res.status(404).json("No orders yet");
+    }
+    res.status(200).json(getOrders);
+  } catch (error) {
+    const err = new Error(error.message);
+    return next(err);
   }
-  res.status(200).json(getOrders)
-} catch (error) {
-    const err = new Error(error.message)
-    return next(err)  
-}
-}
-const getPendingOrders=async(req,res,next)=>{
-try {
-  const pending= await Orders.findAll({where:{status: "pending"}})
-  if (!pending) {
-    return res.status(404).json("No pending order")
+};
+const getPendingOrders = async (req, res, next) => {
+  try {
+    const pending = await Orders.findAll({ where: { status: "pending" } });
+    if (!pending) {
+      return res.status(404).json("No pending order");
+    }
+    res.status(200).json(pending);
+  } catch (error) {
+    const err = new Error(error.message);
+    return next(err);
   }
-  res.status(200).json(pending)
-} catch (error) {
-  const err = new Error(error.message)
-  return next(err) 
-}
-}
-const getOrderById =async(req,res,next) =>{
-    const {id} = req.params
- try {
-    const order = await Orders.findByPk(id)
+};
+const getOrderById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const order = await Orders.findByPk(id);
 
     if (!order) {
-        return res.status(404).json("Order not found")
-      }
-      res.status(200).json(order)
-
- } catch (error) {
-    const err = new Error(error.message)
-    return next(err)
- }
-}
-
-
-let orderUpdate
-
-
-const updateOrder =async(req,res,next)=>{
-    const {id} = req.params
-    try {
-        const order= await Orders.update(req.body,{where:{id}})
-        if (order.stage ===2) {
-        //emit io
-        }
-       res.status(201).json("stage updated")
-    } catch (error) {
-      const err = new Error(error.message)
-      return next(err)   
+      return res.status(404).json("Order not found");
     }
-}
+    res.status(200).json(order);
+  } catch (error) {
+    const err = new Error(error.message);
+    return next(err);
+  }
+};
 
-const riderOrderHistory = async(req,res,next)=>{
-  const riderName = req.user.name
+let orderUpdate;
+
+const updateOrder = async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const getOrderHistory= await Orders.findAll({where:{status:{[Op.ne]:"in-progress"},rider:riderName}})
+    const order = await Orders.update(req.body, { where: { id } });
+    if (order.stage === 2) {
+      //emit io
+    }
+    res.status(201).json("stage updated");
+  } catch (error) {
+    const err = new Error(error.message);
+    return next(err);
+  }
+};
+
+const riderOrderHistory = async (req, res, next) => {
+  const riderName = req.user.name;
+  try {
+    const getOrderHistory = await Orders.findAll({
+      where: { status: { [Op.ne]: "in-progress" }, rider: riderName },
+    });
 
     if (!getOrderHistory) {
-      const err = new Error("No orders yet")
-      err.status= 404
-      return next(err)
+      const err = new Error("No orders yet");
+      err.status = 404;
+      return next(err);
     }
-    res.status(200).json(getOrderHistory)
+    res.status(200).json(getOrderHistory);
   } catch (error) {
-    const err = new Error(error.message)
-    return next(err) 
+    const err = new Error(error.message);
+    return next(err);
   }
-}
+};
 
-
-const customerOrderHistory = async(req,res,next)=>{
-  const customerName = req.user.name
+const customerOrderHistory = async (req, res, next) => {
+  const customerName = req.user.name;
   try {
-    const getOrderHistory= await Orders.findAll({where:{status:{[Op.ne]:"in-progress"},customer:customerName}})
+    const getOrderHistory = await Orders.findAll({
+      where: { status: { [Op.ne]: "in-progress" }, customer: customerName },
+    });
 
     if (!getOrderHistory) {
-      const err = new Error("No orders yet")
-      err.status= 404
-      return next(err)
+      const err = new Error("No orders yet");
+      err.status = 404;
+      return next(err);
     }
-    res.status(200).json(getOrderHistory)
+    res.status(200).json(getOrderHistory);
   } catch (error) {
-    const err = new Error(error.message)
-    return next(err) 
+    const err = new Error(error.message);
+    return next(err);
   }
-}
+};
 
-
-const riderOngoingOrder = async(req,res,next)=>{
-  const riderName = req.user.name
+const riderOngoingOrder = async (req, res, next) => {
+  const riderName = req.user.name;
   try {
-    const ongoingOrder= await Orders.findOne({where:{status:"in-progress",rider:riderName}})
+    const ongoingOrder = await Orders.findOne({
+      where: { status: "in-progress", rider: riderName },
+    });
 
     if (!ongoingOrder) {
-      const err = new Error("No orders yet")
-      err.status= 404
-      return next(err)
+      const err = new Error("No orders yet");
+      err.status = 404;
+      return next(err);
     }
-    const ongoingRiderOrder= []
-    ongoingRiderOrder.push(ongoingOrder)
+    const ongoingRiderOrder = [];
+    ongoingRiderOrder.push(ongoingOrder);
 
-    res.status(200).json(ongoingRiderOrder)
+    res.status(200).json(ongoingRiderOrder);
   } catch (error) {
-    const err = new Error(error.message)
-    return next(err) 
+    const err = new Error(error.message);
+    return next(err);
   }
-}
+};
 
-const customerOngoingOrder = async(req,res,next)=>{
-  const customerName = req.user.name
+const customerOngoingOrder = async (req, res, next) => {
+  const customerName = req.user.name;
   try {
-    const ongoingOrder= await Orders.findOne({where:{status:"in-progress",customer:customerName}})
+    const ongoingOrder = await Orders.findOne({
+      where: { status: "in-progress", customer: customerName },
+    });
 
     if (!ongoingOrder) {
-      const err = new Error("No orders yet")
-      err.status= 404
-      return next(err)
+      const err = new Error("No orders yet");
+      err.status = 404;
+      return next(err);
     }
-    res.status(200).json(ongoingOrder)
+    res.status(200).json(ongoingOrder);
   } catch (error) {
-    const err = new Error(error.message)
-    return next(err) 
+    const err = new Error(error.message);
+    return next(err);
   }
-}
-module.exports={
-  getAllOrders
-  ,getOrderById,
+};
+module.exports = {
+  getAllOrders,
+  getOrderById,
   getPendingOrders,
   createOrder,
   acceptOrder,
@@ -204,5 +427,5 @@ module.exports={
   riderOrderHistory,
   riderOngoingOrder,
   customerOngoingOrder,
-  customerOrderHistory
-}
+  customerOrderHistory,
+};
--- server/utils/socketConnection.js
+++ server/utils/socketConnection.js
@@ -1,65 +1,111 @@
-const { Server } = require("socket.io");
-const { orderUpdate } = require("../controllers/order");
+// const { Server } = require("socket.io");
+// const { orderUpdate } = require("../controllers/order");
 
-let io;
+// let io;
 
-let onlineCustomers = [];
+// let onlineCustomers = [];
 
-const addUser = (name, socketId) => {
-  !onlineCustomers.some((user) => user.name === name) &&
-    onlineCustomers.push({ name, socketId });
-};
-const getCustomer = (name) => {
-  return onlineCustomers.find((customer) => customer.name === name);
-};
-const disconnect = (id) => {
-  onlineCustomers = onlineCustomers.filter(
-    (customer) => customer.socketId !== id
-  );
-};
-const initSocket = (instant) => {
-  if (!io) {
-    io = new Server(instant, {
-      cors: {
-        origin: "http://localhost:5173",
-        methods: [" GET", "POST"],
-        allowedHeaders: ["content-type"],
-        credentials: true,
-      },
+// const addUser = (name, socketId) => {
+//   !onlineCustomers.some((user) => user.name === name) &&
+//     onlineCustomers.push({ name, socketId });
+// };
+// const getCustomer = (name) => {
+//   return onlineCustomers.find((customer) => customer.name === name);
+// };
+// const disconnect = (id) => {
+//   onlineCustomers = onlineCustomers.filter(
+//     (customer) => customer.socketId !== id
+//   );
+// };
+// const initSocket = (instant) => {
+//   if (!io) {
+//     io = new Server(instant, {
+//       cors: {
+//         origin: "http://localhost:5173",
+//         methods: [" GET", "POST"],
+//         allowedHeaders: ["content-type"],
+//         credentials: true,
+//       },
+//     });
+//     io.on("connection", (socket) => {
+//       console.log("connected");
+
+//       //join room
+//       socket.on("join_room", (data) => {
+//         socket.join(data.room);
+//         console.log(`customer joined room ${data.room}`);
+//         if (data.role === "rider") {
+//           socket.to(data.room).emit("to_customer", data);
+//           console.log(data);
+//         }
+//       });
+
+//       socket.on("from_rider", (data) => {
+//         socket.to(data.room).emit("to_customer", data);
+//         console.log("rider sent to customer");
+//       });
+
+//       socket.on("disconnect", () => {
+//         console.log(`${socket.id} disconnected`);
+//         disconnect(socket.id);
+//       });
+//     });
+//   }
+
+//   return io;
+// };
+
+// const getIo = () => {
+//   if (!io) {
+//     throw new Error("socket io not initialized");
+//   }
+//   return io;
+// };
+
+// module.exports = { initSocket, getIo, onlineCustomers, getCustomer };
+
+/* UPDATED CODES */
+
+const socketConnection = (io) => {
+  io.on("connection", (conn) => {
+    console.log("User connected with socket ID:", conn.id);
+    conn.on("joinRoom", (payload) => {
+      if (!payload) {
+        conn.emit("error", "Invalid payload: Payload is undefined");
+        console.error("Received undefined payload in joinRoom event");
+        return;
+      }
+
+      const { customerName, role } = payload;
+      const missingFields = [];
+
+      if (!customerName) missingFields.push("customerName");
+      if (!role) missingFields.push("role");
+
+      if (missingFields.length > 0) {
+        conn.emit(
+          "joinRoom",
+          `Invalid payload: Missing fields - ${missingFields.join(", ")}`
+        );
+        console.error(
+          `joinRoom payload is missing fields: ${missingFields.join(", ")}`
+        );
+        return;
+      }
+
+      conn.join(customerName);
+      console.log(
+        `${role} with Name ${customerName} joined room: ${customerName}`
+      );
+      conn.emit("joinRoom", `Joined ${role} room with Name: ${customerName}`);
     });
-    io.on("connection", (socket) => {
-      console.log("connected");
 
-      //join room
-      socket.on("join_room", (data) => {
-        socket.join(data.room);
-        console.log(`customer joined room ${data.room}`);
-        if (data.role === "rider") {
-          socket.to(data.room).emit("to_customer", data);
-          console.log(data);
-        }
-      });
-
-      socket.on("from_rider", (data) => {
-        socket.to(data.room).emit("to_customer", data);
-        console.log("rider sent to customer");
-      });
-
-      socket.on("disconnect", () => {
-        console.log(`${socket.id} disconnected`);
-        disconnect(socket.id);
-      });
+    conn.on("disconnect", () => {
+      console.log("User disconnected");
     });
-  }
-
-  return io;
+  });
 };
 
-const getIo = () => {
-  if (!io) {
-    throw new Error("socket io not initialized");
-  }
-  return io;
-};
+module.exports = socketConnection;
 
-module.exports = { initSocket, getIo, onlineCustomers, getCustomer };
+/*  */
