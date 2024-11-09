25ade7aa5d17dd447ffbcb4c45f208423032d9cf
marcusxart <cemeji64@gmail.com>
2024-11-09 15:42:05 +0100

    update

--- server/configs/constants.js
+++ server/configs/constants.js
@@ -1,3 +1,7 @@
-const AlLOWED_ORIGINS = ["http://localhost:5500", "http://localhost:5173"];
+const AlLOWED_ORIGINS = [
+  "http://localhost:5500",
+  "http://localhost:5173",
+  "https://dropshop-landing.onrender.com",
+];
 
 module.exports = { AlLOWED_ORIGINS };
