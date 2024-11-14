32551ffd78aa589b3fb0d81002e3a61ec3ab6cde
Dhanielgeek <dhanielknightz46@gmail.com>
2024-11-14 11:23:01 +0100

    testinhg done

--- server/configs/constants.js
+++ server/configs/constants.js
@@ -1,13 +1,7 @@
-
-const AlLOWED_ORIGINS = ["http://localhost:5500", "http://localhost:5173","https://dropshop-landing.onrender.com/"];
-
-module.exports = { AlLOWED_ORIGINS };
-
-const AlLOWED_ORIGINS = [
+const ALLOWED_ORIGINS = [
   "http://localhost:5500",
   "http://localhost:5173",
   "https://dropshop-landing.onrender.com",
 ];
 
-module.exports = { AlLOWED_ORIGINS };
-
+module.exports = { ALLOWED_ORIGINS };
