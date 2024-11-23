95a525eb758b471c8ef819486852f71df05d7f1f
Dhanielgeek <dhanielknightz46@gmail.com>
2024-11-23 12:35:04 +0100

    testing the app...

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,52 +1,52 @@
 // App.js
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
-import { useEffect } from "react";
-import { io } from "socket.io-client";
-import { useDispatch, useSelector } from "react-redux";
-import { setOrderStatus, setStage } from "./Global/Orderstatus";
+// import { useEffect } from "react";
+// import { io } from "socket.io-client";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setOrderStatus, setStage } from "./Global/Orderstatus";
 
-const socket = io("http://localhost:5000");
+// const socket = io("https://dropshop-server.onrender.com");
 
 export default function App() {
-  const customerdata = useSelector((state) => state.customer.orders);
+  // const customerdata = useSelector((state) => state.customer.orders);
 
-  console.log(customerdata.customer);
+  // console.log(customerdata.customer);
 
-  const orderStatus = useSelector((state) => state.OrderStatus.status);
-  console.log(orderStatus);
+  // const orderStatus = useSelector((state) => state.OrderStatus.status);
+  // console.log(orderStatus);
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (!socket.connected) {
-      socket.connect();
-    }
+  // useEffect(() => {
+  //   if (!socket.connected) {
+  //     socket.connect();
+  //   }
 
-    socket.on("connect", () => {
-      console.log("Socket connected:", socket.id);
+  //   socket.on("connect", () => {
+  //     console.log("Socket connected:", socket.id);
 
-      if (socket.connected && customerdata.name) {
-        socket.emit("joinRoom", {
-          customerName: customerdata.name,
-          role: "customer",
-        });
-      }
+  //     if (socket.connected && customerdata.name) {
+  //       socket.emit("joinRoom", {
+  //         customerName: customerdata.name,
+  //         role: "customer",
+  //       });
+  //     }
 
-      socket.on("orderStatusUpdate", (data) => {
-        console.log("Order status update received:", data);
-        dispatch(setOrderStatus(data));
-      });
-      socket.on("updateOrderStatus", (data) => {
-        console.log("Order status change to this :", data);
-        dispatch(setStage(data));
-      });
-    });
+  //     socket.on("orderStatusUpdate", (data) => {
+  //       console.log("Order status update received:", data);
+  //       dispatch(setOrderStatus(data));
+  //     });
+  //     socket.on("updateOrderStatus", (data) => {
+  //       console.log("Order status change to this :", data);
+  //       dispatch(setStage(data));
+  //     });
+  //   });
 
-    return () => {
-      socket.disconnect();
-    };
-  }, [customerdata.name, dispatch]);
+  //   return () => {
+  //     socket.disconnect();
+  //   };
+  // }, [customerdata.name, dispatch]);
 
   return (
     <>
