e473d4128d784c1b5656cdb0a3c12014f50d6acd
Kerry kaiso <kerryesua9@gmail.com>
2024-11-23 09:01:03 +0100

    added url

--- server/configs/constants.js
+++ server/configs/constants.js
@@ -2,6 +2,7 @@ const ALLOWED_ORIGINS = [
   "http://localhost:5500",
   "http://localhost:5173",
   "https://dropshop-landing.onrender.com",
+  "https://dropshop-app.onrender.com/auth"
 ];
 
 module.exports = { ALLOWED_ORIGINS };
