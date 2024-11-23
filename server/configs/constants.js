9f50eca92a596089d9ab787a06c12a439775829c
Kerry kaiso <kerryesua9@gmail.com>
2024-11-23 13:24:30 +0100

    cors 2

--- server/configs/constants.js
+++ server/configs/constants.js
@@ -1,6 +1,6 @@
 const ALLOWED_ORIGINS = [
   "http://localhost:5500",
-  "http://localhost:5173",
+  "http://localhost:5173/auth",
   "https://dropshop-landing.onrender.com",
   "https://dropshop-app.onrender.com/auth"
 ];
