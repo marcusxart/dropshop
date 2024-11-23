76eab754a021fd0f20cc7fe9e882003c9cbd388f
marcusxart <cemeji64@gmail.com>
2024-11-23 14:19:32 +0100

    update cors

--- server/configs/constants.js
+++ server/configs/constants.js
@@ -1,6 +1,8 @@
 const AlLOWED_ORIGINS = [
   "http://localhost:5500",
   "http://localhost:5173",
+  "localhost:5173",
+  "127:0:0:1:5173",
   "https://dropshop-landing.onrender.com",
   "https://dropshop-app.onrender.com",
 ];
